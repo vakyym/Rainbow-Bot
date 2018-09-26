9a378efbcec02adc201a648a891ecff8acf95273
vuoto <binxieboy666@gmail.com>
2018-09-25 23:39:23 -0400

    Base

---
+++ Rainbow/Core.cs
@@ -0,0 +1,27 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Role.Handlers;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+namespace Role.Core
+{
+    public class Core
+    {
+        public static void Main(string[] args) => new Core().LoginAsync().GetAwaiter().GetResult();
+
+        public async Task LoginAsync()
+        {
+            var services = new ServiceCollection()
+            .AddSingleton(new DiscordSocketClient(new DiscordSocketConfig
+            {
+                MessageCacheSize = 50,
+                AlwaysDownloadUsers = true,
+                LogLevel = LogSeverity.Verbose
+            }))
+            .AddSingleton<CoreHandler>();
+            var provider = services.BuildServiceProvider();
+            await provider.GetRequiredService<CoreHandler>().InitializeAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
---
+++ Rainbow/CoreHandler.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Linq;
+namespace Role.Handlers
+{
+    public class CoreHandler
+    {
+        public static DiscordSocketClient Client;
+        static Timer Timer;
+        public CoreHandler(DiscordSocketClient client)
+        {
+            Client = client;
+        }
+        public async Task InitializeAsync()
+        {
+            await Client.LoginAsync(TokenType.Bot, "NDkwOTMwOTYzMzQyNDI2MTEz.DoAq2g.KtsIt06xiFu9pxi-LAgLiqOhb_I").ConfigureAwait(false);
+            await Client.StartAsync().ConfigureAwait(false);
+            StartRoleService();
+        }
+        internal static void StartRoleService()
+        {
+            try
+            {
+                Timer = new Timer(_ =>
+                {
+                    Task.Run(async () =>
+                    {
+                        var Guild = Client.GetGuild(334099272473509898) as SocketGuild;
+                        var Role = Guild.Roles.Where(x => x.Name == "?").FirstOrDefault() as SocketRole;
+                        await Role.ModifyAsync(x => x.Color = new Color(240, 62, 62));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        await Role.ModifyAsync(x => x.Color = new Color(238, 134, 64));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        await Role.ModifyAsync(x => x.Color = new Color(236, 222, 113));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        await Role.ModifyAsync(x => x.Color = new Color(97, 223, 97));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        await Role.ModifyAsync(x => x.Color = new Color(80, 161, 236));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        await Role.ModifyAsync(x => x.Color = new Color(164, 110, 199));
+                        Thread.Sleep(15000);
+                        Console.WriteLine("Succesfully changed color");
+                        Console.WriteLine("### RESTARTING SERVICE ###");
+                    });
+                }, null, TimeSpan.FromMilliseconds(105000), TimeSpan.FromMilliseconds(105000));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
